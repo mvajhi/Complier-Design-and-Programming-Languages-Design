8a6eda18b2d06da8ef9b820cc12f85a9d8e95a0a
mvajhi <mvajhimv@gmail.com>
2024-11-14 15:05:53 +0330

    Start phase 1

---
+++ src/SOACT.java
@@ -0,0 +1,14 @@
+import main.grammar.*;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import java.io.IOException;
+
+public class SOACT {
+    public static void main(String[] args) throws IOException {
+        CharStream reader = CharStreams.fromFileName(args[0]);
+        SOACTLexer lexer = new SOACTLexer(reader);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        SOACTParser parser = new SOACTParser(tokens);
+        ParseTree tree = parser.program();
+    }
+}
