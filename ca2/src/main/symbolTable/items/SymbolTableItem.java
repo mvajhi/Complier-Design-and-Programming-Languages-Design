7ae01f17ae2b12f46dcb46bb34e3b5db762ca5e8
mvajhi <mvajhimv@gmail.com>
2024-12-21 09:10:10 +0330

    added type and actorDec

--- ca2/src/main/symbolTable/items/SymbolTableItem.java
+++ ca2/src/main/symbolTable/items/SymbolTableItem.java
@@ -1,7 +1,13 @@
 package main.symbolTable.items;
 
+import main.ast.nodes.declaration.ActorDec;
+import main.ast.nodes.type.Type;
+
 public abstract class SymbolTableItem {
     protected String name;
+    private ActorDec actorDec;
+    private Type type;
+
     public abstract String getKey();
 
     public String getName() {
@@ -10,5 +16,21 @@ public abstract class SymbolTableItem {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setActorDec(ActorDec actorDec){
+        this.actorDec = actorDec;
+    }
+
+    public ActorDec getActorDec(){
+        return actorDec;
+    }
+
+    public void setType(Type type){
+        this.type = type;
+    }
+
+    public Type getType(){
+        return type;
     }
 }
